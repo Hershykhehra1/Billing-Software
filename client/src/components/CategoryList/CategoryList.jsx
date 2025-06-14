8682be9f975ff336747e4b89fa773341922050d9
Harshaun <harshaunkhehra@yahoo.com>
2025-06-13 21:14:29 -0700

    created the category list component for the user to be able to see/search users and delete them

--- client/src/components/CategoryList/CategoryList.jsx
+++ client/src/components/CategoryList/CategoryList.jsx
@@ -74,7 +74,6 @@ const CategoryList = () => {
           </div>
         ))}
       </div>
-
     </div>
   )
 }
--- client/src/components/UserForm/UserForm.jsx
+++ client/src/components/UserForm/UserForm.jsx
@@ -39,12 +39,10 @@ const UserForm = ({setUsers}) => {
     }
   }
 
-
-  
   return (
     <div className="mx-2 mt-2">
       <div className="row">
-        <div className="card col-md-8 form-container">
+        <div className="card col-md-12 form-container">
           <div className="card-body">
             <form onSubmit={onSubmitHandler}>
               {/* name */}
