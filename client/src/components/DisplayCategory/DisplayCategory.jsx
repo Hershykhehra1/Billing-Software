c34b22231e51a581ac2b695eafedcf438d81f05a
Harshaun <harshaunkhehra@yahoo.com>
2025-06-14 22:31:22 -0700

    added the displayitems section of the explore page

--- client/src/components/DisplayCategory/DisplayCategory.jsx
+++ client/src/components/DisplayCategory/DisplayCategory.jsx
@@ -1,18 +1,28 @@
 import './DisplayCategory.css';
 import Category from '../Category/Category.jsx';
 
-const DisplayCategory = ({categories, selectedCategory, setSelectedCategory}) => {
+const DisplayCategory = ({ categories, selectedCategory, setSelectedCategory }) => {
+  const handleCategoryClick = (categoryId) => {
+    // If the clicked category is already selected, unselect it
+    if (selectedCategory === categoryId) {
+      setSelectedCategory(null);
+    } else {
+      // Otherwise, select the new category
+      setSelectedCategory(categoryId);
+    }
+  };
+
   return (
-    <div className="row g-3" style={{width: '100%' , margin: 0 }}>
+    <div className="row g-3" style={{ width: '100%', margin: 0 }}>
       {categories.map((category) => (
-        <div className="col-md-3 col-sm-6" key={category.categoryId} style={{padding: '0 10px'}}>
+        <div className="col-md-3 col-sm-6" key={category.categoryId} style={{ padding: '0 10px' }}>
           <Category
-              categoryName={category.name}
-              imgUrl={category.imgUrl}
-              numberOfItems={category.items}
-              bgColor={category.bgColor} 
-              isSelected={selectedCategory === category.categoryId}
-              onClick={() => setSelectedCategory(category.categoryId)}/>
+            categoryName={category.name}
+            imgUrl={category.imgUrl}
+            numberOfItems={category.items}
+            bgColor={category.bgColor}
+            isSelected={selectedCategory === category.categoryId}
+            onClick={() => handleCategoryClick(category.categoryId)} />
         </div>
       ))}
     </div>
